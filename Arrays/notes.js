f0e3cba368a9a6220ff6a92fda9afd26257d6380
Jack Cooper <JCooper@executivegrapevine.com>
2019-01-10 17:08:43 +0000

    Objects section Complete. Array directory added

---
+++ Arrays/notes.js
@@ -0,0 +1,12 @@
+const notes = ['Note 1', 'Note 2', 'Note 3'];
+console.log(notes.length);
+console.log(notes[0]);
+
+notes.pop();
+notes.push('My new note');
+
+
+// access the last item in an array
+// console.log(notes[notes.length - 1]); // note 3 - prints last item in array 
+console.log(notes);
+
---
+++ Arrays/todo.js
@@ -0,0 +1,7 @@
+// create 5 todo's
+// print message - you have X todos - X is array
+// print the first and second to last values of the array
+const todos = ['play piano', 'code', 'cook', 'run', 'sleep'];
+console.log(`You have ${todos.length} todos in your list`);
+console.log(`First Todo - ${todos[0]} : Second to Last Todo - ${todos[todos.length - 2]}`);
+
