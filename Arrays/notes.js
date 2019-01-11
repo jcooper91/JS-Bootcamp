ce2b4d677a88d57ac93e6e010e02cebe6d32579a
Jack Cooper <JCooper@executivegrapevine.com>
2019-01-11 15:20:25 +0000

    Objects complte. Started arrays

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -2,8 +2,12 @@ const notes = ['Note 1', 'Note 2', 'Note 3'];
 console.log(notes.length);
 console.log(notes[0]);
 
-notes.pop();
+console.log(notes.pop()); // returns last array element
 notes.push('My new note');
+
+console.log(notes.shift()); // remove first item
+console.log(notes.unshift('My first note'));
+// add item at beginnig of array
 
 
 // access the last item in an array
