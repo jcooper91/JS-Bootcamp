0a997a4ae3393f358b4c4093e79860be392f3a43
Jack Cooper <JCooper@GRAPEVINE.local>
2019-01-25 17:21:24 +0000

    todo and note app filtering added

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,6 +1,26 @@
+const notes = [
+    {
+        title: 'My next trip',
+        body: 'Off to Vienna'
+    }, {
+        title: 'Resolutions this year',
+        body: 'Exercise'
+    }, {
+        title: 'Office Modification',
+        body: 'Get a new mouse'
+    }];
+
+
 const p = document.querySelector('p');
 const ps = document.querySelectorAll('p');
 ps.forEach(function(p) {
     p.innerText = '****';
     // p.remove();
 })
+
+
+// add a new element
+// apendChild places the item in the last element of the section your picking. So when selecting body, it would be the last element in the body. REMEMBER!
+let newParagraph = document.createElement('p');
+newParagraph.innerText = 'This is a new element by Javascript';
+document.querySelector('body').appendChild(newParagraph);
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,6 +1,58 @@
-const ps = document.querySelectorAll('p');
-ps.forEach(function(p) {
-    if(p.textContent.includes('the')) {
-        p.remove();
+const todos = [
+    {
+        text: 'Play piano',
+        complete: true
+    },
+    {
+        text: 'Code',
+        complete: true
+    },
+    {
+        text: 'Cook',
+        complete: false
+    },
+    {
+        text: 'Run',
+        complete: false
+    },
+    {
+        text: 'Sleep',
+        complete: true
     }
+]
+
+const inCompleteTodos = todos.filter(function(todo) {
+    return !todo.complete;
+}) 
+
+const summary = document.createElement('h2');
+summary.innerText = `You have ${inCompleteTodos.length} todo's left`;
+document.querySelector('body').appendChild(summary);
+
+
+todos.forEach(function(todo) {
+    const todoPara = document.createElement('p');
+    todoPara.innerHTML = todo.text;
+    document.querySelector('body').appendChild(todoPara);
+    console.log(todo.text)
 })
+
+// you have two todos left ni a paragraph tag
+// print a paragraph for each todo above (use text value for the object)
+
+
+
+
+
+
+
+
+
+
+// const ps = document.querySelectorAll('p');
+
+// ps.forEach(function(p) {
+//     if(p.textContent.includes('the')) {
+//         p.remove();
+//     }
+// })
