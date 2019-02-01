a8816c4b870f2c21bf7e0ce794f4350105fe2acd
Jack_EG <JCooper@executive-grapevine.com>
2019-02-01 17:35:27 +0000

    filtering for notes and todo.

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -14,6 +14,21 @@ const filters = {
     searchText: ''
 }
 
+// const user = {
+//     name: 'Jack',
+//     age: 27
+// }
+
+// const userJSON = JSON.stringify(user);
+// console.log(userJSON);
+// localStorage.setItem('user', userJSON);
+
+const userJSON = localStorage.getItem('user');
+const user = JSON.parse(userJSON);
+console.log(`${user.name} is ${user.age}`);
+
+
+
 const renderNotes = function(notes, filters) {
     const filteredNotes = notes.filter(function(note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
@@ -40,6 +55,5 @@ document.querySelector('#create-note').addEventListener('click', function(e) {
 })
 
 document.querySelector('#filter-by').addEventListener('change', function(e) {
-    console.log(e.target.value);
-    
+    console.log(e.target.value);  
 })
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -21,45 +21,66 @@ const todos = [
     }
 ]
 
-
- const filters = {
-     searchText: ''
- }
-
-// renderTodos = filter by search text in input & filter by complte (searchTextMatch & hideCompleteMatch respectively)
-// filter inCompleteTodos
-// print incomplete todos to a h2
-// list the todos in a list of p
-// add a todo to the array
-// query/eventlisteners - hide-completed, search-todo, todo-form 
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
 
 const renderTodos = function(todos, filters) {
 
-    const filteredTodos = todos.filter(function(todo) {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-        return searchTextMatch;
+    let filterTodos = todos.filter(function(todo) {
+       const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+       const hideCompleteMatch = !filters.hideCompleted || !todo.complete;
+       return searchTextMatch && hideCompleteMatch; 
     })
     
-    document.querySelector('#todo-list').innerHTML = ''; 
+    const inCompleteTodos = filterTodos.filter(function(todo) {
+       return !todo.complete;
+   }) 
 
-    filteredTodos.forEach(function(todo) {
-        const newEl = document.createElement('p');
-        newEl.innerText = todo.text;
-        document.querySelector('#todo-list').appendChild(newEl);
-    })
+   document.querySelector('#todo-list').innerHTML = '';
+   
+   // print incomplete todo's
+   const summary = document.createElement('h2');
+   summary.textContent = `You have ${inCompleteTodos.length} todo's left`;
+   document.querySelector('#todo-list').appendChild(summary);
+   
+   
+   filterTodos.forEach(function(todo) {
+       const todoPara = document.createElement('p');
+       todoPara.innerHTML = todo.text;
+       document.querySelector('#todo-list').appendChild(todoPara);
+   })
 }
 
-renderTodos(todos, filters);
+renderTodos(todos, filters)
 
-document.querySelector('#search-todo').addEventListener('input', function(e) {
-    filters.searchText = e.target.value;
-    renderTodos(todos, filters)
+document.querySelector('#hide-completed').addEventListener('change', function(e) {
+   filters.hideCompleted = e.target.checked;
+   renderTodos(todos, filters)
 })
 
 
+document.querySelector('#search-todo').addEventListener('input', function(e) {
+    filters.searchText = e.target.value;
+    renderTodos(todos, filters);
+})
 
 
+document.querySelector('#todo-form').addEventListener('submit', function(e) {
+   e.preventDefault();
+    todos.push({
+       text:  e.target.elements.newTodo.value,
+       complete: false
+    }) 
+   e.target.elements.newTodo.value = '';
+   renderTodos(todos, filters)
+  
+})
 
+document.querySelector('#create-todo').addEventListener('click', function(e) {
+   e.target.textContent = 'The button was clicked!';
+})
 
 
 
